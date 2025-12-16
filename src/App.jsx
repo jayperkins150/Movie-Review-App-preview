6f6888038bbdb973e13b7858888a69c2875ea420
jayperkins150 <jayperkins150@yahoo.co.uk>
2025-12-16 15:38:19 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import Header from "./assets/components/Header";
+import Footer from "./assets/components/Footer";
+import Movies from "./assets/components/Movies/Movies";
+
+function App() {
+  return (
+    <div className="app-container">
+      
+      <Header />
+
+      <main className="main-content">
+        <h1>Welcome to the Movie Review App</h1>
+        <p>This is the main content area where movie listings or reviews will appear.</p>
+        <Movies />
+      </main>
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
